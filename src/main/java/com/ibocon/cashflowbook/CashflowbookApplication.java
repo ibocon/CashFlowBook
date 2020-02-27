953dd3cbd5aa4fdd5646503a7001968131ecdd07
Yegun Kim <wlfka102@gmail.com>
2020-02-27 23:03:38 +0900

    CAS-7 Spring Initializr 로 초기 프로젝트 생성

---
+++ src/main/java/com/ibocon/cashflowbook/CashflowbookApplication.java
@@ -0,0 +1,13 @@
+package com.ibocon.cashflowbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibocon/cashflowbook/CashflowbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibocon.cashflowbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
