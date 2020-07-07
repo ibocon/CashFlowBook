1f9439e7223fb2621670b1734b51f4944e04ba20
Yegun Kim <wlfka102@gmail.com>
2020-07-07 23:39:53 +0900

    Typescript "React & Webpack" 적용

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -20,6 +20,21 @@ module.exports = {
   })],
   module: {
     rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+              {
+                  loader: "ts-loader"
+              }
+          ]
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        },
         // js jsx
         { 
             test: /\.(js|jsx)$/,
@@ -64,4 +79,12 @@ module.exports = {
         }
     ]
   },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
 };
